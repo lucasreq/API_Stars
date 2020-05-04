3d05876101feed00c1b05e3e1e0f3f1b5d64e5ca
lucasreq <lucas.requena@ynov.com>
2020-05-04 22:17:39 +0200

    pug change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Cards = require('./models/Cards');
 const bodyparser = require('body-parser')
 var path = require('path');
+
 //const Sequelize = require('sequelize');
 
 const app = express();
@@ -14,10 +15,11 @@ app.use(bodyparser.urlencoded({ extended: true}))
 app.use(bodyparser.json())
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.use('/', indexRouter);
 app.use('/cartes',apiRestRouter);
+  
 
 app.use(function(req, res, next) {
     next(createError(404));
