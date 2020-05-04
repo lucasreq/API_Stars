4045e791febf7545abeb3d5ca0c0dc3df1eaeaad
lucasreq <lucas.requena@ynov.com>
2020-05-04 23:46:08 +0200

    readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# API-Star
+*Probably a api on the card game Star-Realms*
+
+## installation
+```
+$ git clone https://github.com/lucasreq/API_Stars.git
+$ cd ./API_Stars/
+$ npm install
+$ npm start
+```
+
+## navigation
+show all
+```/```
+show, update, delete card by id
+```/cartes/:id```
+post card
+```/cartes```
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 const Cards = require('../models/Cards');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 
 /* GET home page. */
-router.get('/',function(req,res,next){
+router.get('/',function(req,res){
   let filter = {};
   let { q } = req.query;
 
